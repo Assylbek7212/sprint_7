562fce879d8df8dd230e463566f647256d00f8fb
Assylbek Balmukhanov <assylbek.balmukhanov@gmail.com>
2024-09-12 12:04:53 +0200

    fix:test_create_courier_missing_fields

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -29,12 +29,28 @@ class TestCreateCourier:
 
     @allure.title("Создание курьера с отсутствующими полями")
     def test_create_courier_missing_fields(self):
-        payload = {
-            "login": generate_random_string(10),
-            "password": generate_random_string(10)
+        # Отсутствует поле "login"
+        payload_missing_login = {
+            "password": generate_random_string(10),
+            "firstName": generate_random_string(10)
         }
-        response = requests.post(f'{BASE_URL}/courier', json=payload)
-        assert response.status_code == 400
+        response_missing_login = requests.post(f'{BASE_URL}/courier', json=payload_missing_login)
+        assert response_missing_login.status_code == 400
+
+        # Отсутствует поле "password"
+        payload_missing_password = {
+            "login": generate_random_string(10),
+            "firstName": generate_random_string(10)
+        }
+        response_missing_password = requests.post(f'{BASE_URL}/courier', json=payload_missing_password)
+        assert response_missing_password.status_code == 400
+
+        # Отсутствуют оба поля
+        payload_missing_both = {
+            "firstName": generate_random_string(10)
+        }
+        response_missing_both = requests.post(f'{BASE_URL}/courier', json=payload_missing_both)
+        assert response_missing_both.status_code == 400
 
     @allure.title("Создание курьера с существующим логином")
     def test_create_courier_existing_login(self):
